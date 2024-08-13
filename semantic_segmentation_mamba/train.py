bf86de0fb804ce79574bc2c78946a893e9bbf8e0
yonghuname <etgituse@163.com>
2024-08-13 23:55:30 +0800

    5层mamba来了等待完善

--- semantic_segmentation_mamba/train.py
+++ semantic_segmentation_mamba/train.py
@@ -89,7 +89,7 @@ def train_net(dataset_name):
     # net = RSM_SS2hw(dims=ph.dims, depths=ph.depths, ssm_d_state=ph.ssm_d_state, ssm_dt_rank=ph.ssm_dt_rank,
     #              ssm_ratio=ph.ssm_ratio, mlp_ratio=ph.mlp_ratio, downsample_version=ph.downsample_version,
     #              patchembed_version=ph.patchembed_version)
-    net = mambaunet5deepnet(depths=[5, 5, 5,5,5],      dims=[96, 192, 384, 768,14], ssm_d_state=ph.ssm_d_state, ssm_dt_rank=ph.ssm_dt_rank,
+    net = mambaunet5deepnet(depths=[5, 5, 5,5 ,5],      dims=[96, 192, 384, 768,14], ssm_d_state=ph.ssm_d_state, ssm_dt_rank=ph.ssm_dt_rank,
                  ssm_ratio=ph.ssm_ratio, mlp_ratio=ph.mlp_ratio, downsample_version=ph.downsample_version,
                  patchembed_version=ph.patchembed_version)
     # net = RSM_SS(dims=ph.dims, depths=ph.depths, ssm_d_state=ph.ssm_d_state, ssm_dt_rank=ph.ssm_dt_rank,  ssm_ratio=ph.ssm_ratio, mlp_ratio=ph.mlp_ratio, downsample_version=ph.downsample_version,  patchembed_version=ph.patchembed_version)
