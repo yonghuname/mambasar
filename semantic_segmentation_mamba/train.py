2be95c2cf5abd04a1c591e418935bb02a33ace6f
yonghuname <etgituse@163.com>
2024-08-14 21:51:17 +0800

    attentiongate in sarv3

--- semantic_segmentation_mamba/train.py
+++ semantic_segmentation_mamba/train.py
@@ -19,7 +19,7 @@ from Utils.utils import train_val_test # 训练和验证函数
 from puremambaunet2 import mygoUNet2,RSM_SS2hw
 from BaseUnet import UNet
 from mambaunet5deep import  mambaunet5deepnet
-
+# from wyxmambaunet import wyxMambaUetwithattention
 # 使用随机种子可以确保每次运行代码时生成的随机数序列相同，从而得到相同的结果
 def random_seed(SEED):
     random.seed(SEED)
@@ -92,6 +92,8 @@ def train_net(dataset_name):
     net = mambaunet5deepnet(depths=[3, 4, 4,4 ,4],      dims=[96, 192, 384, 768,1536], ssm_d_state=ph.ssm_d_state, ssm_dt_rank=ph.ssm_dt_rank,
                  ssm_ratio=ph.ssm_ratio, mlp_ratio=ph.mlp_ratio, downsample_version=ph.downsample_version,
                  patchembed_version=ph.patchembed_version)
+    # net = wyxMambaUetwithattention(  num_classes=1,   in_channels=4,     img_size=256,      embed_dim=768,      depth=12,     patch_size=16  )
+
     # net = RSM_SS(dims=ph.dims, depths=ph.depths, ssm_d_state=ph.ssm_d_state, ssm_dt_rank=ph.ssm_dt_rank,  ssm_ratio=ph.ssm_ratio, mlp_ratio=ph.mlp_ratio, downsample_version=ph.downsample_version,  patchembed_version=ph.patchembed_version)
     # net = UNet(n_channels=4,n_classes=1,bilinear=True)  # 使用 BaseUNet（经典Unet模型）
     net = net.to(device=device)
