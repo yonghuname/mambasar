646f6728df62a9285ef3d3a4fa06d7e99c7ddb7b
yonghuname <etgituse@163.com>
2024-08-16 19:43:47 +0800

    多头注意力

--- semantic_segmentation_mamba/res4mambaunetmore.py
+++ semantic_segmentation_mamba/res4mambaunetmore.py
@@ -1396,14 +1396,15 @@ class EncoderLayer(nn.Module):
         x = self.residual_blocks(x)
 
         print(f"xsize",x.size())
-        x = x.permute(0, 1, 3, 2)
+
 
         print(f"xsize22222",x.size())
         # print(x.shape())
-        # x=self.mha(x)
+        x=self.mha(x)
         x= self.dab(x)
-
+        attn_output = x.permute(0, 3, 1, 2).contiguous()
         print(f"xsize",x.size())
+
         return x
 
 class EncoderLayer2(nn.Module):
